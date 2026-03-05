449bb107bdaf0d1be79b929b2a29736fcea81d04
Abdellatif Feghouli <wesbedouinson@gmail.com>
2026-03-06 01:55:12 +0300

    Add solution for Reverse String (trial 1)

--- leetcode/reverse-string_trial1.py
+++ leetcode/reverse-string_trial1.py
@@ -3,6 +3,5 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        for i in range(len(s)//2):
-            s[i], s[-i-1] = s[-i-1], s[i]
+        s.reverse()
         
