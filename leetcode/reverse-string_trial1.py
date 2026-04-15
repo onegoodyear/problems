23228b2689768001e0133f6f48c98667aa83288f
Abdellatif Feghouli <wesbedouinson@gmail.com>
2026-04-15 18:34:11 +0300

    Add solution for Reverse String (trial 1)

--- leetcode/reverse-string_trial1.py
+++ leetcode/reverse-string_trial1.py
@@ -3,5 +3,10 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s.reverse()
+        def reverseStringRec(s: List[str], beg: int, end: int) -> None:
+            if beg < end:
+                s[beg], s[end] = s[end], s[beg]
+                reverseStringRec(s, beg + 1, end - 1)
+        reverseStringRec(s, 0, len(s)-1)
+
         
