65a5394a69e340735d6d5f13a7e4752a26edd1ca
Abdellatif Feghouli <wesbedouinson@gmail.com>
2026-04-30 21:14:21 +0300

    Add solution for Binary Tree Inorder Traversal (trial 1)

---
+++ leetcode/binary-tree-inorder-traversal_trial1.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def inorder(r: TreeNode):
+            if not r: return
+            inorder(r.left)
+            res.append(r.val)
+            inorder(r.right)
+        
+        inorder(root)
+        return res
+
+
+        
